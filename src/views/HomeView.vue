8c33524c2126c8d87ce73fa0df2d6a365c162192
Yurii Rybachok <drummer2009@ukr.net>
2023-03-18 18:14:33 +0200

    add serviceSelectLarge component for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="home">
-    HOME
+  <div id="Home" class="flex flex-col h-screen">
+    <div class="m-3 mb-auto">
+      <div class="grid grid-cols-2 gap-3 my-3">
+        <ServiceSelectLarge 
+          text="Ride"
+          imageWidth="74"
+          image="ride"
+        />
+        <ServiceSelectLarge 
+          text="Food"
+          imageWidth="72"
+          image="food"
+          :promo="true"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import ServiceSelectLarge from "@/components/ServiceSelectLarge.vue";
 </script>
