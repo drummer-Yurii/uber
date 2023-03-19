ea7e196f96bc309f689d982a71c3d67b88a34df8
Yurii Rybachok <drummer2009@ukr.net>
2023-03-19 13:15:09 +0200

    directions

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,7 +4,7 @@
     <div id="VehicleSelection" class="w-full">
       <div class="w-full h-2 border-t"></div>
       <div class="w-full text-center border-t-2 p-1.5 text-gray-700 text-lg font-semibold">
-        Distance - 50
+        Distance - {{ distance.text }}
       </div>
       <div class="scrollSection">
         <div class="bg-custom-gray">
@@ -13,9 +13,9 @@
             <div class="w-full ml-3">
               <div class="flex items-center justify-between">
                 <div class="text-2xl mb-1">UberX</div>
-                <div class="text-xl">$55.50</div>
+                <div class="text-xl">{{ distance.value }}</div>
               </div>
-              <div class="text-gray-500">7 hours</div>
+              <div class="text-gray-500">{{ duration.text }}</div>
             </div>
           </div>
         </div>
@@ -25,36 +25,53 @@
             <div class="w-full ml-3">
               <div class="flex items-center justify-between">
                 <div class="text-2xl mb-1">UberX</div>
-                <div class="text-xl">$55.50</div>
+                <div class="text-xl">{{ distance.value }}</div>
               </div>
-              <div class="text-gray-500">7 hours</div>
+              <div class="text-gray-500">{{ duration.text }}</div>
             </div>
           </div>
         </div>
-        <div>
-          <div class="flex items-center px-4 py-5">
-            <img width="75" src="img/uber/uberxl.png" alt="">
-            <div class="w-full ml-3">
-              <div class="flex items-center justify-between">
-                <div class="text-2xl mb-1">UberX</div>
-                <div class="text-xl">$55.50</div>
-              </div>
-              <div class="text-gray-500">7 hours</div>
+      <div>
+        <div class="flex items-center px-4 py-5">
+          <img width="75" src="img/uber/uberxl.png" alt="">
+          <div class="w-full ml-3">
+            <div class="flex items-center justify-between">
+              <div class="text-2xl mb-1">UberX</div>
+              <div class="text-xl">{{ distance.value }}</div>
             </div>
+            <div class="text-gray-500">{{ duration.text }}</div>
           </div>
         </div>
-      </div>
-      <div class="flex items-center justify-center bg-white py-6 px-4 w-full absolute bottom-0 shadow-inner">
-        <button class="bg-black text-2xl text-white py-4 px-4 rounded-sm w-full">
-          Confirm UberX
-        </button>
       </div>
     </div>
+    <div class="flex items-center justify-center bg-white py-6 px-4 w-full absolute bottom-0 shadow-inner">
+      <button class="bg-black text-2xl text-white py-4 px-4 rounded-sm w-full">
+        Confirm UberX
+      </button>
+    </div>
   </div>
-</template>
+</div></template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import { useDirectionStore } from '@/store/direction-store';
+
+const direction = useDirectionStore();
+
+const distance = ref({ text: '', value: null });
+const duration = ref({ text: '', value: null });
+
+const latLng = ref({
+  start: {
+    lat: null,
+    lng: null,
+  },
+  end: {
+    lat: null,
+    lng: null,
+  },
+})
 
 onMounted(() => {
   setTimeout(() => {
@@ -63,15 +80,58 @@ onMounted(() => {
 })
 
 const initMap = () => {
+  const directionsService = new window.google.maps.DirectionService()
+  const directionsRenderer = new window.google.maps.DirectionRenderer()
+
+  directionsRenderer.setOptions({
+    polylineOptions: {
+      strokeColor: '#212121',
+      strokeWeight: 6
+    }
+  })
+
   const map = new window.google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    minZoom: 3,
+    maxZoom: 17,
     fullscreenControl: false,
     zoomControl: false,
     streetViewControl: false,
     mapTypeControl: false,
   });
-  console.log(map);
+  if (direction.pickup && direction.destination) {
+    getDirections(map, directionsRenderer, directionsService)
+    getDistance()
+  }
+}
+
+const getDirections = (map, directionsRenderer, directionsService) => {
+  directionsRenderer.setMap(map)
+
+  const request = {
+    origin: direction.pickup,
+    destination: direction.destination,
+    optimizeWaypoints: true,
+    travelMode: 'DRIVING'
+  }
+  directionsService.route(request, (result, status) => {
+    if (status === 'OK') {
+      latLng.value.start.lat = result.routes[0].legs[0].start_location.lat()
+      latLng.value.start.lng = result.routes[0].legs[0].start_location.lng()
+      latLng.value.end.lat = result.routes[0].legs[0].end_location.lat()
+      latLng.value.end.lng = result.routes[0].legs[0].end_location.lng()
+
+      directionsRenderer.setDirections(result);
+    }
+  })
+}
+
+const getDistance = async () => {
+  let res = await axios.get('distance/' + direction.pickup + '/' + direction.destination)
+  distance.value.text = res.data.rows[0].elements[0].distance.text
+  distance.value.value = res.data.rows[0].elements[0].distance.value
+  duration.value.text = res.data.rows[0].elements[0].duration.text
+  duration.value.value = res.data.rows[0].elements[0].duration.value
 }
 
 </script>
@@ -92,6 +152,7 @@ const initMap = () => {
   .bg-custom-gray {
     background-color: rgb(237, 237, 237);
   }
+
   #VehicleSelection {
     .scrollSection {
       height: calc(50vh - 120px);
